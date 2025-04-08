ef0489004d89e35d952ca52c45aaac72eafd9599
Ryan Zaken <queakie24@gmail.com>
2025-04-08 18:53:19 -0400

    Admin Creating Profiles

--- src/main/java/view/AdminHomeController.java
+++ src/main/java/view/AdminHomeController.java
@@ -1,10 +1,14 @@
 package view;
 
 import app.Photos;
-import app.model.User;
 import app.model.UserList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class AdminHomeController {
+
+  @FXML
+  private TextField usernameField;
 
   private Photos app;
   private UserList userList;
@@ -12,6 +16,16 @@ public class AdminHomeController {
   public void init(Photos app, UserList userList) {
     this.app = app;
     this.userList = userList;
+  }
+
+  @FXML
+  public void handleCreateUser (){
+    try {
+      userList.addUser(usernameField.getText().trim());
+    } catch (Exception e) {
+      System.out.println("Error creating user: " + e.getMessage());
+    }
+
   }
 
 }
